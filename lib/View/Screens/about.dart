da06f719a10c794bb855fb7b609243d14f6e8b82
Sagar Dhadke <peaceunique2003@gmail.com>
2024-10-26 13:08:53 +0530

    Added Screen

---
+++ lib/View/Screens/about.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAbout extends StatefulWidget {
+  const MyAbout({super.key});
+
+  @override
+  State<MyAbout> createState() => _MyAboutState();
+}
+
+class _MyAboutState extends State<MyAbout> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/View/Screens/products.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyProducts extends StatefulWidget {
+  const MyProducts({super.key});
+
+  @override
+  State<MyProducts> createState() => _MyProductsState();
+}
+
+class _MyProductsState extends State<MyProducts> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
