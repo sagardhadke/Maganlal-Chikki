353a768a0bc32f97d82a3509046dc7ab9a80c6f7
Sagar Dhadke <peaceunique2003@gmail.com>
2024-10-26 13:07:14 +0530

    Added Bottom Navigation Bar

--- lib/View/dashBoard.dart
+++ lib/View/dashBoard.dart
@@ -8,8 +8,32 @@ class MyDashboard extends StatefulWidget {
 }
 
 class _MyDashboardState extends State<MyDashboard> {
+  int myIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bottom Nav"),
+        backgroundColor: Colors.orange,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.amber,
+          onTap: (index) {
+            setState(() {
+              myIndex = index;
+            });
+          },
+          currentIndex: myIndex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category_rounded), label: 'Category'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_basket_outlined), label: 'Products'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.info_outlined), label: 'About')
+          ]),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MyDashboard()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MyDashboard());
   }
 }
